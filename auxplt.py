5f4a65602c28b652364fdf14d84ab81aa23aebe1
rtagirov <tagirovrinat@gmail.com>
2018-05-13 16:28:01 +0100

    add auxfunc.py auxplt.py paths.py

--- auxplt.py
+++ auxplt.py
@@ -1,1 +1,40 @@
-../src/aux/auxplt.py
+from pylab                           import rcParams
+from matplotlib                      import rc
+from matplotlib.backends.backend_pdf import PdfPages
+
+#import seaborn as sns
+import numpy as np
+
+import importlib
+
+import paths; importlib.reload(paths)
+
+def figpar(xtick_maj_pad = 15, ytick_maj_pad = 10, fontsize = 10):
+
+#    sns.set(rc={"figure.figsize": figsize})
+#    np.random.seed(sum(map(ord, "palettes")))
+
+#    seaborn.palplot(seaborn.color_palette('cubehelix', 11))
+
+#    seaborn.palplot(seaborn.diverging_palette(255, 133, l=60, n=11, center='dark'))
+
+#    sns.palplot(sns.color_palette('dark'))
+
+#    sns.set(font_scale = 1.5)
+
+#    sns.set_style(style = 'white')
+
+    rcParams['xtick.major.pad'] = xtick_maj_pad
+    rcParams['ytick.major.pad'] = ytick_maj_pad
+
+    rc('font', size = fontsize)
+#    rc('font', family = 'serif')
+#    rc('font', serif = 'Times')
+#    rc('text', usetex = True)
+
+def savepdf(filename, figdir = paths.figdir):
+
+    pp = PdfPages(figdir + filename + '.pdf')
+
+    pp.savefig(bbox_inches = 'tight')
+    pp.close()
--- paths.py
+++ paths.py
@@ -1,1 +1,38 @@
-../src/aux/paths.py
+import socket
+
+hostname = socket.gethostname()
+
+if hostname == 'ph-rtagirov': sim = '/mnt/SSD/sim'
+
+if hostname == 'schiphol':    sim = '/Users/rinattagirov/sim'
+
+npz =    sim + '/python/npz/'
+
+it0h =   sim + '/runs/hminus/IT0/'
+it1h =   sim + '/runs/hminus/IT1/'
+it2h =   sim + '/runs/hminus/IT2/'
+it3h =   sim + '/runs/hminus/IT3/'
+
+it0f =   sim + '/runs/fioss/IT0/'
+it1f =   sim + '/runs/fioss/IT1/'
+it2f =   sim + '/runs/fioss/IT2/'
+it3f =   sim + '/runs/fioss/IT3/'
+
+figdir = sim + '/python/fig/'
+
+inp =    sim + '/python/inp/'
+out =    sim + '/python/out/'
+
+idlinp = sim + '/idl/input/'
+idlout = sim + '/idl/output/'
+
+pydir =  sim + '/python/src/'
+
+atlruns = sim + '/runs/atlas9/'
+
+lev = 'NLTE/LEV/'
+
+satnlt = sim + '/satire_nlte/'
+satlte = sim + '/satire_lte/'
+satrnm = sim + '/satire_rnm/'
+satnss = sim + '/satire_nlte_ss/'
